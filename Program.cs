4c3d6b7e429f34c9bbd965a0ac0b2189699a44d8
Lucas Moser <moser.lucas@gmail.com>
2021-03-17 13:25:58 -0400

    Updates to handling of formatters

--- Program.cs
+++ Program.cs
@@ -15,37 +15,43 @@ namespace CST371
 
     public static class Program
     {
-        private static Args ParseArgs(string[] args)
+        public static Args ParseArgs(string[] args)
         {
             string query = args[0];
+            string format = args.Length == 1 ? null : args[1];
             return new Args(){
                 Cuisine = query.Split("near")[0],
                 Address = query.Split("near")[1],
-                Format = args[1]
+                Format = format
             };
+        }
+
+        public static IFormatter GetFormatter(Args a, string defaultFormat)
+        {
+            string format = a.Format??defaultFormat;
+
+            if (format == "CSV")
+                return new CsvFormatter();
+            else if (format == "Natural")
+                return new NaturalFormatter();
+            else if (format == "JSON")
+                return new JsonFormatter();
+            else
+                throw new NotImplementedException($"Format {a.Format} specified but no implementation found");
         }
 
         public static async Task Main(string[] args)
         {
-            Args a = ParseArgs(args);
-
             IConfiguration config = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", true, true)
                 .Build();
 
+            Args a = ParseArgs(args);
+
             IGoogleApiClient client= new GoogleApiClientComposed(config["apikey"]);
             var placesRes = await client.GetFoodNearAddress(a.Cuisine, a.Address).ConfigureAwait(false);
 
-            IFormatter formatter;
-            if(a.Format == "CSV")
-                formatter = new CsvFormatter();
-            else if (a.Format == "Natural")
-                formatter = new NaturalFormatter();
-            else if (a.Format == "JSON")
-                formatter = new JsonFormatter();
-            else
-                throw new NotImplementedException($"Format {a.Format} specified but no implementation found");
-
+            var formatter = GetFormatter(a, config["defaultFormat"]);
             string formattedResults = formatter.Format(placesRes);
 
             Console.WriteLine(formattedResults);
