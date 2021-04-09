0381b49525814ad7442eb0032943806e9186c46b
Lucas Moser <moser.lucas@gmail.com>
2021-04-09 09:13:53 -0400

    Adding web

---
+++ RestaurantFinderWeb/Controllers/RestaurantFinderController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CST371.GoogleApi;
+using CST371.ResultFilter;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace RestaurantFinderWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RestaurantFinderController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _config;
+
+        public RestaurantFinderController(ILogger<WeatherForecastController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpGet("/restaurants/")]
+        public async Task<PlacesResponse> Get([FromQuery] string address, [FromQuery] string cuisine)
+        {
+            IGoogleApiClient client= new GoogleApiClientComposed(_config["apikey"]);
+            return await client.GetFoodNearAddress(cuisine, address);
+        }
+    }
+}
